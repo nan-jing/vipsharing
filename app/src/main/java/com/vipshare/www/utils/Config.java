eaef45a058d47359d42a0adbd6c65e6b2e74e862
cjzang <719671022@qq.com>
2018-06-17 21:57:14 +0800

    Revert "Title: Submit the first Code Project"
    This reverts commit 3d16a17cbd9f748ef9a39cf6657f88bd239b0684.

--- app/src/main/java/com/vipshare/www/utils/Config.java
+++
@@ -1,6 +0,0 @@
-package com.vipshare.www.utils;
-
-public class Config {
-    public final static String SERVER_ADDRESS = "http://192.168.1.110/";
-    public final static String SERVER_ADDRESS2 = "http://192.168.1.110/get_request.php/";
-}
